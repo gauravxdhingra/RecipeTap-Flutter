fd4c048e7011c0640846b35b5aedfe4c97ae5c24
gauravxdhingra <grvdhingra1999@gmail.com>
2020-05-16 15:54:53 +0530

    Recipe Scraping Finished for both versions of website

--- lib/pages/my_homepage.dart
+++ lib/pages/my_homepage.dart
@@ -14,16 +14,20 @@ class _MyHomePageState extends State<MyHomePage> {
   bool isLoading = true;
 
   final String url =
-      "https://www.allrecipes.com/recipe/262499/tandoori-paneer-tikka-masala/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%201";
+      // "https://www.allrecipes.com/recipe/262499/tandoori-paneer-tikka-masala/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%201";
       // "https://www.allrecipes.com/recipe/129000/caribbean-nachos/?internalSource=staff%20pick&referringId=1228&referringContentType=Recipe%20Hub&clickId=cardslot%201#nutrition";
       // "https://www.allrecipes.com/recipe/127491/easy-oreo-truffles/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%202";
+      "https://www.allrecipes.com/recipe/228899/palak-paneer/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%201";
   bool oldWebsite;
 
   String headline;
   String desc;
+  String time;
+  String servings;
   List ingredients = [];
   List directions = [];
   List nutritionalFacts = [];
+  List nutritionalFactsNew = [];
   List cooksNotes = [];
 
   bool nutritionalFactsExits = false;
@@ -48,6 +52,21 @@ class _MyHomePageState extends State<MyHomePage> {
       headline =
           document.getElementsByClassName("headline heading-content")[3].text;
 // margin-0-auto
+
+      // 0 prep
+      // 1 cook
+      // 2 total
+      // 3 servings
+      // 4. Servings
+
+      time = document
+          .getElementsByClassName("recipe-meta-item-body")[2]
+          .text
+          .trim();
+      servings = document
+          .getElementsByClassName("recipe-meta-item-body")[3]
+          .text
+          .trim();
 
       desc = document.getElementsByClassName("margin-0-auto")[0].text.trim();
 
@@ -89,6 +108,18 @@ class _MyHomePageState extends State<MyHomePage> {
       headline = document.getElementsByClassName("recipe-summary__h1")[0].text;
 // margin-0-auto
 
+      time = document.getElementsByClassName("ready-in-time")[0].text.trim();
+      // final servingss = document
+      //     .getElementsByClassName("recipe-ingredients ng-scope");
+
+      final servingss = document
+          .querySelector('[ng-controller="ar_controllers_recipe_ingredient"]')
+          .attributes["ng-init"]
+          .split("(")[1]
+          .split(",")[0];
+      servings = servingss;
+      print(servingss);
+
       desc = document
           .getElementsByClassName("submitter__description")[0]
           .text
@@ -111,29 +142,58 @@ class _MyHomePageState extends State<MyHomePage> {
       // subcontainer instructions-section-item
 
 //TODO
-      // nutritionalFactsExits = document
-      //     .getElementsByClassName("partial recipe-nutrition-section")
-      //     .isNotEmpty;
+      nutritionalFactsExits =
+          document.getElementsByClassName("nutrition-summary-facts").isNotEmpty;
 
-      // if (nutritionalFactsExits) {
-      //   String nutritionText = directions[directions.length - 1];
-      //   List<String> roughNutri = nutritionText.split("  ");
-      //   roughNutri.forEach((element) {
-      //     if (element != '') nutritionalFacts.add(element.trim());
-      //   });
-      // }
+      if (nutritionalFactsExits) {
+        document.getElementsByClassName("nutrition-summary-facts");
+        // print(nutritionalFactsNew[0]);
+        // 1. calories
+        // 2. Fat
+        // 3. Carbohydrates
+        // 4. Protien
+        // 5. Cholestrol
+        // 6. Sodium
 
-      // nutritionalFacts = roughNutri;
-      final selector = document.getElementsByClassName("recipe-footnotes");
-      cooksNotesExits = selector.isNotEmpty;
-      if (selector.isNotEmpty)
-        selector.forEach((element) {
+        nutritionalFactsNew.add(document
+            .querySelector('[itemprop="calories"]')
+            .text
+            .replaceAll(";", ""));
+        nutritionalFactsNew.add(
+            document.querySelector('[itemprop="fatContent"]').text + "g fat");
+        nutritionalFactsNew.add(
+            document.querySelector('[itemprop="carbohydrateContent"]').text +
+                "g carbohydrates");
+        nutritionalFactsNew.add(
+            document.querySelector('[itemprop="proteinContent"]').text +
+                "g protein");
+        nutritionalFactsNew.add(
+            document.querySelector('[itemprop="cholesterolContent"]').text +
+                "mg cholestrol");
+        nutritionalFactsNew.add(
+            document.querySelector('[itemprop="sodiumContent"]').text +
+                "mg sodium");
+      }
+
+      // Footer notes
+      final selector = document.getElementsByClassName("recipe-footnotes__h4");
+      // recipe-footnotes__header
+      cooksNotesExits = selector[0].text.trim() == "Footnotes";
+      if (selector.isNotEmpty) {
+        document.getElementsByClassName("recipe-footnotes").forEach((element) {
           final inote = element.text.trim();
-          if (inote != '' && inote != "Footnotes") {
-            cooksNotes.add(inote);
-            cooksNotes = cooksNotes[0].toString().split("  ");
+          if (inote != '' || inote != "Footnotes") {
+            cooksNotes.add(inote.trim());
+            // cooksNotes = cooksNotes[0].toString().split("  ");
           }
         });
+        cooksNotes[0] = cooksNotes[0]
+            .toString()
+            .substring(10)
+            .trim()
+            .split("                ");
+        print('ckn' + cooksNotes[0].length.toString());
+      }
     }
     setState(() {
       print(headline);
@@ -150,6 +210,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print(cooksNotes);
     // getData();
     return Scaffold(
       floatingActionButton: FloatingActionButton(
@@ -165,6 +226,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: <Widget>[
                   // Description
                   Text(desc),
+                  // Time
+                  Text(time),
+                  // Servings
+                  Text(servings),
                   // Ingredients
                   Container(
                     height: 200,
@@ -192,14 +257,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     Container(
                       height: 100,
                       // child: Text(directions[directions.length - 1]),
-                      child: ListView.builder(
-                        itemCount: nutritionalFacts.length - 1,
-                        itemBuilder: (BuildContext context, int index) {
-                          print(nutritionalFacts[index].toString().trimRight());
-                          return Text(
-                              nutritionalFacts[index].toString().trimRight());
-                        },
-                      ),
+                      child: oldWebsite
+                          ? ListView.builder(
+                              itemCount: nutritionalFacts.length - 1,
+                              itemBuilder: (BuildContext context, int index) {
+                                print(nutritionalFacts[index]
+                                    .toString()
+                                    .trimRight());
+                                return Text(nutritionalFacts[index]
+                                    .toString()
+                                    .trimRight());
+                              },
+                            )
+                          : ListView.builder(
+                              itemCount: nutritionalFactsNew.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                print(nutritionalFactsNew[index]
+                                    .toString()
+                                    .trim());
+                                return Text(nutritionalFactsNew[index]
+                                    .toString()
+                                    .trim());
+                              },
+                            ),
                     ),
                   // extra cooks notes
                   if (cooksNotesExits)
@@ -209,13 +289,22 @@ class _MyHomePageState extends State<MyHomePage> {
                     // ),
                     Container(
                       height: 100,
-                      child: ListView.builder(
-                        itemCount: cooksNotes.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          print(cooksNotes.length);
-                          return Text(cooksNotes[index]);
-                        },
-                      ),
+                      child: oldWebsite
+                          ? ListView.builder(
+                              itemCount: cooksNotes.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                print(cooksNotes.length);
+                                return Text(cooksNotes[index]
+                                    .toString()
+                                    .substring(20)
+                                    .trim());
+                              })
+                          : ListView.builder(
+                              itemCount: cooksNotes[0].length,
+                              itemBuilder: (BuildContext context, int index) {
+                                print(cooksNotes[0].length);
+                                return Text(cooksNotes[0][index].trim());
+                              }),
                     ),
                 ],
               ),
