dbd1516007b11bb42667e98ee86cae4c063273b0
gauravxdhingra <grvdhingra1999@gmail.com>
2020-05-16 10:48:52 +0530

    Scraping for old website added

--- lib/pages/my_homepage.dart
+++ lib/pages/my_homepage.dart
@@ -15,8 +15,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final String url =
       // "https://www.allrecipes.com/recipe/262499/tandoori-paneer-tikka-masala/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%201";
-      "https://www.allrecipes.com/recipe/129000/caribbean-nachos/?internalSource=staff%20pick&referringId=1228&referringContentType=Recipe%20Hub&clickId=cardslot%201#nutrition";
-
+      // "https://www.allrecipes.com/recipe/129000/caribbean-nachos/?internalSource=staff%20pick&referringId=1228&referringContentType=Recipe%20Hub&clickId=cardslot%201#nutrition";
+      "https://www.allrecipes.com/recipe/127491/easy-oreo-truffles/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%202";
   bool oldWebsite;
 
   String headline;
@@ -37,8 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // oldWebsite =
 
     try {
-      document.getElementsByClassName(
-          "recipes-txt {active:topBrowseRecipePanel_showing} ng-isolate-scope");
+      document.getElementsByClassName("magazine-bar__social")[0].text;
       oldWebsite = false;
     } catch (e) {
       print('old website');
@@ -102,32 +101,35 @@ class _MyHomePageState extends State<MyHomePage> {
         ingredients.add(iingred);
       });
 
-      document.getElementsByClassName("section-body").forEach((element) {
+      document
+          .getElementsByClassName("recipe-directions__list--item")
+          .forEach((element) {
         final istep = element.text.trim();
         directions.add(istep);
       });
       // section-body
       // subcontainer instructions-section-item
 
-      nutritionalFactsExits = document
-          .getElementsByClassName("partial recipe-nutrition-section")
-          .isNotEmpty;
+//TODO
+      // nutritionalFactsExits = document
+      //     .getElementsByClassName("partial recipe-nutrition-section")
+      //     .isNotEmpty;
 
-      if (nutritionalFactsExits) {
-        String nutritionText = directions[directions.length - 1];
-        List<String> roughNutri = nutritionText.split("  ");
-        roughNutri.forEach((element) {
-          if (element != '') nutritionalFacts.add(element.trim());
-        });
-      }
+      // if (nutritionalFactsExits) {
+      //   String nutritionText = directions[directions.length - 1];
+      //   List<String> roughNutri = nutritionText.split("  ");
+      //   roughNutri.forEach((element) {
+      //     if (element != '') nutritionalFacts.add(element.trim());
+      //   });
+      // }
 
       // nutritionalFacts = roughNutri;
-      final selector = document.getElementsByClassName("recipe-note container");
+      final selector = document.getElementsByClassName("recipe-footnotes");
       cooksNotesExits = selector.isNotEmpty;
       if (selector.isNotEmpty)
         selector.forEach((element) {
           final inote = element.text.trim();
-          if (inote != '' && inote != "Cook's Notes:") cooksNotes.add(inote);
+          if (inote != '' && inote != "Footnotes") cooksNotes.add(inote);
         });
     }
     setState(() {
@@ -155,63 +157,54 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: isLoading
           ? CircularProgressIndicator()
-          : oldWebsite
-              ? SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      // Description
-                      Text(desc),
-                      // Ingredients
-                      Container(
-                        height: 250,
-                        child: ListView.builder(
-                          itemCount: ingredients.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Text(ingredients[index]);
-                          },
-                        ),
-                      ),
-                      // directons/steps
-                      Container(
-                        height: 250,
-                        child: ListView.builder(
-                          itemCount: directions.length - 1,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Text(directions[index]);
-                          },
-                        ),
-                      ),
-                      // nutritional facts
-                      if (nutritionalFactsExits)
-                        Container(
-                          height: 100,
-                          // child: Text(directions[directions.length - 1]),
-                          child: ListView.builder(
-                            itemCount: nutritionalFacts.length - 1,
-                            itemBuilder: (BuildContext context, int index) {
-                              print(nutritionalFacts[index]
-                                  .toString()
-                                  .trimRight());
-                              return Text(nutritionalFacts[index]
-                                  .toString()
-                                  .trimRight());
-                            },
-                          ),
-                        ),
-                      // extra cooks notes
-                      if (cooksNotesExits)
-                        Container(
-                          child: Text(
-                              cooksNotes[0].toString().substring(20).trim()),
-                        ),
-                    ],
+          : SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  // Description
+                  Text(desc),
+                  // Ingredients
+                  Container(
+                    height: 250,
+                    child: ListView.builder(
+                      itemCount: ingredients.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Text(ingredients[index]);
+                      },
+                    ),
                   ),
-                )
-              : Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.black,
-                ),
+                  // directons/steps
+                  Container(
+                    height: 250,
+                    child: ListView.builder(
+                      itemCount: directions.length - 1,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Text(directions[index]);
+                      },
+                    ),
+                  ),
+                  // nutritional facts
+                  if (nutritionalFactsExits)
+                    Container(
+                      height: 100,
+                      // child: Text(directions[directions.length - 1]),
+                      child: ListView.builder(
+                        itemCount: nutritionalFacts.length - 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          print(nutritionalFacts[index].toString().trimRight());
+                          return Text(
+                              nutritionalFacts[index].toString().trimRight());
+                        },
+                      ),
+                    ),
+                  // extra cooks notes
+                  if (cooksNotesExits)
+                    Container(
+                      child:
+                          Text(cooksNotes[0].toString().substring(20).trim()),
+                    ),
+                ],
+              ),
+            ),
     );
   }
 }
